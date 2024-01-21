c4889cded2f8be658645bcd9651a0123b404132b
Sangodare Isaac <sangodareisaac92@gmail.com>
2024-01-21 20:03:31 +0100

    why roboadvisor

--- roboadvisor/src/components/advisoradvantage/Advisoradvantage.jsx
+++ roboadvisor/src/components/advisoradvantage/Advisoradvantage.jsx
@@ -9,11 +9,11 @@ export const Advisoradvantage = () => {
   return (
     
     <div className="w-full md:pt-[100px] md:pb-[50px] md:px-[97px] px-5 h-max relative">
-      <div className="pb-[50px] pt-5 md:pt-0">
+      <div className="pb-[10px] md:pb-[50px] pt-5 md:pt-0">
         <div className="text-blue-700 text-sm font-bold">
           The RoboAdvisor Advantage
         </div>
-        <div className="text-gray-800 font-[500] md:text-[2.5rem] my-4 mx-0 -tracking-[0.96px] ">
+        <div className="text-gray-800 font-[500] text-2xl md:text-[2.5rem] my-2 md:my-4 mx-0 -tracking-[0.96px] ">
           Why choose RoboAdvisor?
         </div>
         <div className="text-[14px] md:text-[17px] font-normal md:leading-8 ">
@@ -48,8 +48,8 @@ export const Advisoradvantage = () => {
               </div>
             </div>
           </div>
-          <div className="flex w-full h-1/2 gap-[10px] mt-5 md:mt-0">
-            <div className=" w-[48%] h-1/2 px-0 py-[10px]">
+          <div className="flex w-full h-1/2 gap-[10px] mt-7 md:mt-0">
+            <div className=" w-[48%] h-1/2 px-0 pb-20 md:pb-[10px] pt-[10px]">
               <div className=" w-[40px] h-[40px] rounded-full border border-gray-200 bg-gray-50 flex justify-center items-center">
               <GrUserExpert className="text-[#175CD3]"/>
               </div>
@@ -60,21 +60,21 @@ export const Advisoradvantage = () => {
               Our in-house investment team will customize a portfolio to suit your risk tolerance and goals. They keep a close eye on market trends and opportunities, so you don’t have to.
               </div>
             </div>
-            <div className=" w-[48%] h-1/2 px-0 py-[10px]">
+            <div className=" w-[48%] h-1/2 px-0 pb-20 md:pb-[10px] pt-[10px]">
               <div className=" w-[40px] h-[40px] rounded-full border border-gray-200 bg-gray-50 flex justify-center items-center">
                <MdSmartButton className="text-[#175CD3]"/>
               </div>
               <div className=" mt-[10px] text-gray-900 text-[1rem] font-[500] leading-8">
-              Smarter investing, brilliantly personalized.
+              Smart and personalized investment.
 
               </div>
               <p className=" mt-[10px] text-gray-600 text-[0.8rem] font-normal leading-5">
-              Just answer a few questions, and we’ll build you a personalized portfolio of low-cost index funds from up to 17 global asset classes. Our software handles all the trading, rebalancing, and other busywork to help grow your wealth for the long term.
+              A few questions, and we’ll build you a personalized portfolio of low-cost index funds from up to 17 global asset classes. Software automated all the trading, and long term wealth growth.
               </p>
             </div>
           </div>
         </div>
-        <div className=" md:w-[40%] h-full md:mt-0 mt-5">
+        <div className=" md:w-[40%] h-full md:mt-0 mt-[120px]">
           <VideoPlayer src={phonevideo} autoPlay={true} muted loop />
         </div>
       </div>
